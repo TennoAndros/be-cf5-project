719f269cd9645ab4e67040d53869f1fb9a12d803
Andreas <andreasjr77@outlook.com>
2024-06-18 16:18:01 +0300

    completed genre endpoints

--- controllers/genres-controllers.js
+++ controllers/genres-controllers.js
@@ -1,10 +1,21 @@
 const db = require("../db/connection");
-const { selectGenres } = require("../models/genres-models");
+const { selectGenres, insertGenre } = require("../models/genres-models");
 
 exports.getGenres = async (req, res, next) => {
   try {
     const genres = await selectGenres();
     res.status(200).send({ genres });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.postGenre = async (req, res, next) => {
+  try {
+    const genre = req.body;
+    console.log("newGenre1", genre);
+    const newGenre = await insertGenre(genre);
+    res.status(201).send({ newGenre });
   } catch (err) {
     next(err);
   }

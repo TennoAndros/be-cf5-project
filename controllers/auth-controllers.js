8e115ab4bca5a2af42d2e134cd2016e626633b37
Andreas <andreasjr77@outlook.com>
2024-06-28 02:42:39 +0300

    controller cookie change

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -37,7 +37,7 @@ exports.login = async (req, res, next) => {
 
     res.cookie("access_token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production" ? true : false,
+      secure: false,
       sameSite: "none",
     });
 
